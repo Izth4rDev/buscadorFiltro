37471d2ec20e220c3377851005eac85c5ee796fa
Miguel Rondanelli <miguel.rondanelli.inf@gmail.com>
2022-09-28 19:16:52 -0300

    se añade filtrado por marca y año

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
 //Variables
-const resultado = document.querySelector('#resultado');
-
-const años = document.querySelector('#year');
 const max = new Date().getFullYear(); //nos entrega el año actual
 const min = max -122; //año 1900
+
+//datos del formulario
+const marca = document.querySelector('#marca');
+const años = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+
+//contenedor del resultado
+const resultado = document.querySelector('#resultado');
+
+const datosBusqueda = {
+
+    marca:'',
+    year:'',
+    minimo:'',
+    maximo:'',
+    puertas:'',
+    transmision:'',
+    color:''
+
+};
 
 document.addEventListener('DOMContentLoaded', () =>{
 
@@ -13,6 +34,45 @@ document.addEventListener('DOMContentLoaded', () =>{
 
 });
 
+//Event listener para los select de busqueda (filtrado)
+
+marca.addEventListener('change', (e) =>{ //el click tbn funciona =D
+  datosBusqueda.marca = e.target.value;
+  
+  filtrarAuto();
+
+});
+
+años.addEventListener('change', (e) =>{ 
+    datosBusqueda.year = e.target.value;
+
+    filtrarAuto();
+});
+
+minimo.addEventListener('change', (e) =>{ 
+    datosBusqueda.minimo = e.target.value;
+
+});
+
+maximo.addEventListener('change', (e) =>{ 
+    datosBusqueda.maximo = e.target.value;
+
+});
+
+puertas.addEventListener('change', (e) =>{ 
+    datosBusqueda.puertas = e.target.value;
+
+});
+
+transmision.addEventListener('change', (e) =>{ 
+    datosBusqueda.transmision = e.target.value;
+
+});
+
+color.addEventListener('change', (e) =>{ 
+    datosBusqueda.color = e.target.value;
+
+});
 
 //Funciones
 function mostrarAutos (){
@@ -27,7 +87,7 @@ function mostrarAutos (){
         ${marca} ${modelo} - ${year} - ${puertas} Puertas- transmision:${transmision}-Precio:${precio}- ${color} 
 
 
-        `
+        `;
         //insertar en HTML
         
         resultado.appendChild(autoHTML);
@@ -46,8 +106,43 @@ for (let i=max; i>=min; i--){
     option.value = i;
     option.textContent= i;
     años.appendChild(option);
-    
+
 }
 
 
+}
+
+//Funcion que filtra en base a la busqueda
+
+function filtrarAuto(){
+    //se pueden encadenar los filtrados si hay mas de una opcion de filtrado
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear)
+    console.log(resultado);
+}
+
+function filtrarMarca (auto){
+
+    const {marca} = datosBusqueda;
+
+    if(marca){
+       
+        return auto.marca === marca;
+    }
+
+    return auto;
+}
+
+function filtrarYear(auto){
+
+    const {year} = datosBusqueda;
+    //console.log(typeof year); viene como string
+    //console.log(typeof auto.year); viene como numero
+
+    if(year){
+
+        return auto.year === parseInt(year);
+    }
+    
+    return auto;
+    
 }
